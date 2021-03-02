81429c13425aceac0b732969780140b27c267bbe
mtakai <marjorie.takai@gmail.com>
2021-03-02 21:28:29 +0900

    solution to problem ID_001

---
+++ ID_001.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 20:13:39 2021
+
+@author: mtakai
+
+https://projecteuler.net/problem=1
+
+Problem 1
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+"""
+
+x = np.arange(0,1000, 1)
+y = [xi for xi in x if xi % 3 == 0 or xi % 5 ==0]
+print(sum(y))
+
+
